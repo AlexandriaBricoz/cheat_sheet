278fab1fd593b699a9915726b363dc7fe846c273
AlexandriaBricoz <145438833+AlexandriaBricoz@users.noreply.github.com>
2023-10-02 11:59:43 +0500

    Добавил работу с datetime

---
+++ first/age_datetime.py
@@ -0,0 +1,25 @@
+from datetime import date
+
+class Human:
+    age = 0
+    name = ''
+    birthday = 0
+
+    def __init__(self,name,birthday):
+        self.name = name
+        self.birthday = birthday
+        self.age = date.today()-birthday
+    def set_birthday(self,day):
+        self.birthday = day
+        self.age = self.age - day
+
+    def get_birthday(self):
+        return self.birthday
+
+    def get_age(self):
+        return self.age
+
+
+if __name__ == '__main__':
+    student = Human('Don',date(int(input()),int(input()),int(input())))
+    print(student.get_age())
