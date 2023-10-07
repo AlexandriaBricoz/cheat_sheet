923d5c9268c927a8c633e83ba4102ae71fcaa27e
AlexandriaBricoz <145438833+AlexandriaBricoz@users.noreply.github.com>
2023-10-07 16:00:19 +0500

    Добавил работу правило LEGB

---
+++ first/my_LEGB.py
@@ -0,0 +1,16 @@
+#LEGB -rule
+#L-Local
+#E-enclosed
+#G-global
+#B-builtins
+scope = 'global'
+max = 10
+def outer():
+    scope ='enclosed'
+    def inner():
+        scope ='local'
+        print(scope)            # питон ищет переменную или функцию изнутри
+        #print(max([1,2,3]))     # здесь будет ошибка из-за того что max переназначен
+    inner()
+if __name__=='__main__':
+    outer()
