c7bf5e24bb7ea92574f5b698ded4782dc8ec230f
AlexandriaBricoz <145438833+AlexandriaBricoz@users.noreply.github.com>
2023-10-08 23:18:57 +0500

    Добавил работу unittest

--- tasks/task_7.py
+++ tasks/task_7.py
@@ -10,6 +10,10 @@ def func(list):
 
 
 def sort(list):
+    if any([isinstance(e,str) for e in list]):
+        raise ValueError('Введена строка')
+    if not all([isinstance(e,int) for e in list]):
+        raise ValueError('Введены не целые числа')
     array = []
     for namber in range(9,0,-1):
         k = 0
@@ -42,6 +46,7 @@ def sort(list):
 
 if __name__ == '__main__':
     print(func([22,1,54,98,92,9,999,67,5656466,94,35,43,534,5,34,5325,23,45,2345,324,5,3425,342,534,5,34,523,45324,52,36,435,7]))
-    print(func([1,22,94,54,9]))
+    print(sort([12,23,342,90,9,99,967,30]))
+    print(sort([1,111,11,2,12,10]))
     assert func([1,22,94,54,9])==99454221, 'плохо чел'
     assert func([1,22,9,54,94])==99454221, 'плохо чел'
